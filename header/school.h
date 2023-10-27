aaf55e162cff7cd28e4bddef7d1fa8ccd8d98ed2
RadoFitiavana <rakotoarivonyradofitiavana@gmail.com>
2023-10-27 09:21:04 +0300

    mofication des noms d'attributs et des méthodes

--- header/school.h
+++ header/school.h
@@ -9,33 +9,30 @@ class Student
 {
     private :
         int id ; // l'identité de l'étudiant
-        string lname; // le nom de l'élève
-        string fname ; // le prénom
+        string Lname; // le nom de l'élève
+        string Fname ; // le prénom
         string birth ; // la date de naissance
-        string afees ; // la date de paiement du droit de l'élève
-        string Class ; // la classe de l' élève
+        string Afees ; // la date de paiement du droit de l'élève
+        string grade ; // la classe de l' élève
 
     public :
         Student () ; // constructeur 
         int getid () ; 
         void setid () ; 
-        string getlname () ;
-        void setlname () ;
-        string getfname () ;
-        void setfname () ;
+        string getLname () ;
+        void setLname () ;
+        string getFname () ;
+        void setFname () ;
         string getbirth () ;
         void setbirth () ;
-        string getafees () ;
-        void setafees () ;
-        string getClass () ;
-        void setClass () ;
-        void insert () ; // insérer un étudiant dans la base
-        void delete () ; // supprimer un étudiant dans la base
-        void deleteAll () ; // supprimer toutes les listes chaque fin d'année
-        void displayList () ; // afficher la liste des étudiants dans chaque classe  
-        void displayStudent () ; // afficher un étudiant dans la liste
-        void takeFrom () ; //  
-        void saveTo () ; // 
+        string getAfees () ;
+        void setAfees () ;
+        string getgrade () ;
+        void setgrade () ;
+        void insertTodb () ; // insérer un étudiant dans la base
+        void removeFromdb () ; // supprimer un étudiant dans la base  
+        void displayStudent () ; // afficher un étudiant dans la liste  
+        void updateIndb () ; // 
 
 } ;
 
@@ -50,17 +47,16 @@ class Fees
         string mai ;
         string jun ;
         string jul ;
-        string aou ;
-        string sept ;
+        string aug ;
+        string sep ;
         string oct ;
         string nov ;
         string dec ;
 
     public :
         Fees () ; // Constructeur
-        void insert () ; // compléter la date de paiement de l'écolage
-        void delete () ; // supprimer un élève renvoyé
-        void deleteAll () ; // supprimer tous les carnets chaque fin d'année
+        void insertTodb () ; // compléter la date de paiement de l'écolage
+        void removeFrom () ; // supprimer un élève renvoyé
         void displayFees () ; // afficher la liste des carnets de paiement d'écolage 
 } ;
 
